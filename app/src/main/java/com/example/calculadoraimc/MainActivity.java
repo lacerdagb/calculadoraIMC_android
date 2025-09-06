a03072454d92814c979c04c7e6a4d9b6ed0c1f8f
lacerdagb <127674463+lacerdagb@users.noreply.github.com>
2025-09-06 12:18:02 -0300

    configuracao da seekbar

--- app/src/main/java/com/example/calculadoraimc/MainActivity.java
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.calculadoraimc;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,8 +22,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0);
             return insets;
         });
+
+
+        EditText editTextWeight = findViewById(R.id.edittext_weight);
+        SeekBar seekBarHeight = findViewById(R.id.seekbar_height);
+        Button buttonClear = findViewById(R.id.button_clear);
+        Button buttonCalculate = findViewById(R.id.button_calculate);
+        TextView textViewHeight = findViewById(R.id.textview_height);
+
+
+        seekBarHeight.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textViewHeight.setText(progress + "cm");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        buttonClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editTextWeight.setText("");
+                seekBarHeight.setProgress(0);
+            }
+        });
+
     }
 }
